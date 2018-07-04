768d411dad5dea880c88596011664d59382256ca
moshestein <moshestein001@gmail.com>
2018-07-04 19:47:08 -0400

    hw 56 3.0

--- HW56/myApp2.js
+++ HW56/myApp2.js
@@ -1,9 +1,8 @@
-'use strict'
 var myApp = (function (myApp) {
-    var utils = myApp.utils || {};
-    utils.mystringcaseinsensitiveEquals = function (a, b) {
+    'use strict'
+    myApp.utils = myApp.utils || {};
+    myApp.utils.mystringcaseinsensitiveEquals = function (a, b) {
         return a.toLowerCase() === b.toLowerCase();
     }
-    myApp.utils = utils;
     return myApp;
 }(myApp || {}));
