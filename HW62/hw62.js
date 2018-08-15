869e98520d5fe406a0639c8c98e2400378b8d610
moshestein <moshestein001@gmail.com>
2018-08-15 19:56:33 -0400

    x 2

--- HW62/hw62.js
+++ HW62/hw62.js
@@ -3,14 +3,19 @@
     'strict on'
     let c = false;
     $('input[type=button]').css('display', 'none');
-    $('#cb').click(()=>{ 
-        if(c) {
+    $('#cb').click(() => {
+        if (c) {
             $('input[type=button]').css('display', 'none');
-        }else{
+        } else {
             $('input[type=button]').css('display', 'inline');
         }
         c = !c;
     });
-    $('input[type=button]').click(() =>  $.post('example.php', $('#addressForm').serialize) );
+    // $('input[type=button]').click(() => $.post('example.php', $('#addressForm').serialize()));
+    $('input[type=button]').click(() => {
+        $('input[type=text]').each((i, e) => {
+            $('#f').html($(e).html());
+        });
+    });
 
 }());
